2a6dfcb76c001026a3fafedcb6a5da87bba1cb8a
ivanovichi <ivan.arthur18@gmail.com>
2024-05-14 19:39:10 -0600

    javascript code

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
 console.log("Hello world :)");
+let getComputerChoice = Math.floor(Math.random()*3) + 1;     
+console.log(getComputerChoice);
+if (getComputerChoice === 1) {
+    let scissor = "scissor";
+    console.log(scissor);
+} else if (getComputerChoice === 2) {
+    let rock = "rock";
+    console.log(rock);
+} else {
+    let paper = "paper";
+    console.log(paper);
+}
+
+
+
