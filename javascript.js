32c1398cd7d5d7c4e24f850cd23bd934235a1f37
ivanovichi <ivan.arthur18@gmail.com>
2024-05-15 19:51:20 -0600

    Now you can win or lose
    I put the code so that compares the answer of the human and tells you if you win or lose

--- javascript.js
+++ javascript.js
@@ -1,18 +1,22 @@
 console.log("Hello world :)");
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    let rd = Math.floor(Math.random()*3) + 1;     
+    let rd = Math.floor(Math.random()*3) + 1;    
+    let computer = 0; 
 if (rd === 1) {
-    let computer = "scissor";
+    computer = "scissor";
     console.log(computer);
 } else if (rd === 2) {
-    let computer = "rock";
+    computer = "rock";
     console.log(computer);
 } else {
-    let computer = "paper";
+    computer = "paper";
     console.log(computer);
 }
+return computer;
 }
-getComputerChoice();
 
 function getHumanChoice() {
     let humanChoice = prompt("Select between: paper,rock, and scissor");
@@ -27,11 +31,87 @@ function getHumanChoice() {
         alert("Pease type a valid word");
         getHumanChoice();
     }
-
+return human;
 }
-getHumanChoice();
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        alert("Its a tie!!");
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        alert ("Computer wins!!");
+    } else if (humanChoice === "scissor" && computerChoice === "rock") {
+        alert("Computer wins!!");
+    } else if (humanChoice === "paper" && computerChoice === "scissor") {
+        alert("Computer wins!!");
+    } else if (computerChoice === "rock" && humanChoice === "paper") {
+        alert ("Human wins!!");
+    } else if (computerChoice === "scissor" && humanChoice === "rock") {
+        alert("Human wins!!");
+    } else if (computerChoice === "paper" && humanChoice === "scissor") {
+        alert("Human wins!!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(computerSelection);
+console.log(humanSelection);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Hello world :)");
+// let humanScore = 0;
+// let computerScore = 0;
+// function getComputerChoice() {
+//     let rd = Math.floor(Math.random()*3) + 1;     
+// if (rd === 1) {
+//     let computer = "scissor";
+//     console.log(computer);
+// } else if (rd === 2) {
+//     let computer = "rock";
+//     console.log(computer);
+// } else {
+//     let computer = "paper";
+//     console.log(computer);
+// }
+// }
+// getComputerChoice();
+
+// function getHumanChoice() {
+//     let humanChoice = prompt("Select between: paper,rock, and scissor");
+//     let human = humanChoice.toLowerCase();
+//     if (human === "rock") {
+//         console.log(human);
+//     } else if (human === "paper") {
+//         console.log(human);
+//     } else if (human === "scissor") {
+//         console.log(human);
+//     } else {
+//         alert("Pease type a valid word");
+//         getHumanChoice();
+//     }
+
+// }
+// getHumanChoice();
 
 
