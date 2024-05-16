48c009cde712f85837c0dd0c97b936c369927b4a
ivanovichi <ivan.arthur18@gmail.com>
2024-05-15 20:19:45 -0600

    Finish the game
    Improve the code of the human and computer choice
    Set a count of wins
    And the game now is for 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,19 +1,18 @@
-console.log("Hello world :)");
+console.log("Good luck :)");
 let humanScore = 0;
 let computerScore = 0;
+
+playGame();
 
 function getComputerChoice() {
     let rd = Math.floor(Math.random()*3) + 1;    
     let computer = 0; 
 if (rd === 1) {
     computer = "scissor";
-    console.log(computer);
 } else if (rd === 2) {
     computer = "rock";
-    console.log(computer);
 } else {
     computer = "paper";
-    console.log(computer);
 }
 return computer;
 }
@@ -21,12 +20,7 @@ return computer;
 function getHumanChoice() {
     let humanChoice = prompt("Select between: paper,rock, and scissor");
     let human = humanChoice.toLowerCase();
-    if (human === "rock") {
-        console.log(human);
-    } else if (human === "paper") {
-        console.log(human);
-    } else if (human === "scissor") {
-        console.log(human);
+    if (human === "rock" || "paper" || "scissor") {
     } else {
         alert("Pease type a valid word");
         getHumanChoice();
@@ -39,79 +33,41 @@ function playRound(humanChoice, computerChoice) {
         alert("Its a tie!!");
     } else if (humanChoice === "rock" && computerChoice === "paper") {
         alert ("Computer wins!!");
+        computerScore = computerScore + 1;
     } else if (humanChoice === "scissor" && computerChoice === "rock") {
         alert("Computer wins!!");
+        computerScore = computerScore + 1;
     } else if (humanChoice === "paper" && computerChoice === "scissor") {
         alert("Computer wins!!");
+        computerScore = computerScore + 1;
     } else if (computerChoice === "rock" && humanChoice === "paper") {
         alert ("Human wins!!");
+        humanScore = humanScore + 1;
     } else if (computerChoice === "scissor" && humanChoice === "rock") {
         alert("Human wins!!");
+        humanScore = humanScore + 1;
     } else if (computerChoice === "paper" && humanChoice === "scissor") {
         alert("Human wins!!");
+        humanScore = humanScore + 1;
     }
+    console.log("Human score: " + humanScore, "\n");
+    console.log("Computer score: " + computerScore);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
-console.log(computerSelection);
-console.log(humanSelection);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log("Hello world :)");
-// let humanScore = 0;
-// let computerScore = 0;
-// function getComputerChoice() {
-//     let rd = Math.floor(Math.random()*3) + 1;     
-// if (rd === 1) {
-//     let computer = "scissor";
-//     console.log(computer);
-// } else if (rd === 2) {
-//     let computer = "rock";
-//     console.log(computer);
-// } else {
-//     let computer = "paper";
-//     console.log(computer);
-// }
-// }
-// getComputerChoice();
-
-// function getHumanChoice() {
-//     let humanChoice = prompt("Select between: paper,rock, and scissor");
-//     let human = humanChoice.toLowerCase();
-//     if (human === "rock") {
-//         console.log(human);
-//     } else if (human === "paper") {
-//         console.log(human);
-//     } else if (human === "scissor") {
-//         console.log(human);
-//     } else {
-//         alert("Pease type a valid word");
-//         getHumanChoice();
-//     }
-
-// }
-// getHumanChoice();
-
-
+function playGame() {
+    for (let i = 1; i <= 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        console.log(computerSelection);
+        console.log(humanSelection);
+    }
+    if (humanScore > computerScore) {
+        alert("YOU WINS!!!");
+    } else if (computerScore > humanScore) {
+        alert("COMPUTER WINS!!!");
+    } else {
+        alert("Its a tie!!");
+    }
+}
